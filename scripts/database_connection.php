340e4e32d06848d2390b8a9f06b8f14a8c66caa4
Chefus <78282258+chef49@users.noreply.github.com>
2023-01-26 23:20:13 +0100

    Database connection added

--- scripts/database_connection.php
+++ scripts/database_connection.php
@@ -1,1 +1,9 @@
 <?php
+$host = "localhost";
+$port = 5432;
+$db = "travel_agency";
+$user = "postgres";
+$pw = "PW";
+$conStr="pgsql:host=$host;port=$port;dbname=$db;";
+$dbConnection = new PDO( $conStr, $user, $pw );
+?>
